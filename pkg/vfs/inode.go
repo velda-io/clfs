ac2a705a0806a4b78802481774e70ba00900c11a
Chuan Qiu <qiuc12@gmail.com>
2025-07-13 17:17:53 -0700

    Implement more ops

--- pkg/vfs/inode.go
+++ pkg/vfs/inode.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/hanwen/go-fuse/v2/fs"
 	"github.com/hanwen/go-fuse/v2/fuse"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"velda.io/mtfs/pkg/proto"
 )
 
@@ -38,17 +39,20 @@ type Inode struct {
 }
 
 func NewInode(serverProtocol ServerProtocol, cookie []byte, initialSyncGrants int, initialStat *proto.FileStat) *Inode {
-	n := &Inode{
-		serverProtocol: serverProtocol,
-		cookie:         cookie,
-		syncer:         NewSyncer(),
-		cachedStat:     initialStat,
-	}
+	n := &Inode{}
+	n.init(serverProtocol, cookie, initialSyncGrants, initialStat)
+	return n
+}
+
+func (n *Inode) init(serverProtocol ServerProtocol, cookie []byte, initialSyncGrants int, initialStat *proto.FileStat) {
+	n.serverProtocol = serverProtocol
+	n.cookie = cookie
+	n.syncer = NewSyncer()
+	n.cachedStat = initialStat
 	n.syncer.flags = initialSyncGrants
 	if cookie != nil {
 		serverProtocol.RegisterServerCallback(cookie, n.ReceiveResponse)
 	}
-	return n
 }
 
 var _ = (fs.NodeLookuper)((*Inode)(nil))
@@ -185,6 +189,298 @@ func (n *Inode) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut
 		return syscall.EIO
 	}
 }
+
+var _ = (fs.NodeMknoder)((*Inode)(nil))
+
+func (n *Inode) Mknod(ctx context.Context, name string, mode uint32, dev uint32, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
+	op := n.syncer.StartWrite()
+	defer n.syncer.Complete(op)
+	if op.Async() {
+		if n.GetChild(name) != nil {
+			return nil, syscall.EEXIST
+		}
+		newFileStat(ctx, &out.Attr, mode)
+		out.Attr.Rdev = dev
+		stat := StatProtoFromAttr(&out.Attr)
+		node := NewInode(n.serverProtocol, nil, SYNC_EXCLUSIVE_WRITE, stat)
+		n.asyncOperation(ctx, &proto.OperationRequest{
+			Operation: &proto.OperationRequest_Mknod{Mknod: &proto.MknodRequest{
+				Name: name,
+				Stat: stat,
+			}},
+		}, func(response *proto.OperationResponse, err error) {
+			defer n.syncer.CompleteAsync(op)
+			if err != nil {
+				return
+			}
+			switch s := response.Response.(type) {
+			case *proto.OperationResponse_Mknod:
+				cookie := s.Mknod.Cookie
+				node.ResolveCookie(cookie)
+			default:
+				// Handle unexpected response type
+				return
+			}
+		})
+		return n.NewInode(ctx, node, fs.StableAttr{Mode: mode}), 0
+	} else {
+		response, err := n.syncOperation(ctx, &proto.OperationRequest{
+			Cookie: n.cookie,
+			Operation: &proto.OperationRequest_Mknod{Mknod: &proto.MknodRequest{
+				Name: name,
+				Stat: emptyFileStatProto(ctx, mode),
+			}},
+		})
+		if err != nil {
+			return nil, fs.ToErrno(err)
+		}
+		switch s := response.Response.(type) {
+		case *proto.OperationResponse_Mknod:
+			out.Attr = *AttrFromStatProto(s.Mknod.Stat)
+			node := NewInode(n.serverProtocol, s.Mknod.Cookie, SYNC_EXCLUSIVE_WRITE, s.Mknod.Stat)
+			return n.NewInode(ctx, node, fs.StableAttr{Mode: mode}), 0
+		default:
+			log.Printf("Received response with SeqId %d but unknown type: %T, expecting MknodResponse", response.SeqId, s)
+			return nil, syscall.EIO
+		}
+	}
+}
+
+var _ = (fs.NodeRmdirer)((*Inode)(nil))
+
+func (n *Inode) Rmdir(ctx context.Context, name string) syscall.Errno {
+	op := n.syncer.StartWrite()
+	defer n.syncer.Complete(op)
+	if op.Async() {
+		child := n.GetChild(name)
+		if child == nil {
+			return syscall.ENOENT
+		}
+		childNode, ok := child.Operations().(*Inode)
+		if !ok {
+			return syscall.EINVAL
+		}
+		checkChildEmpty := func() bool {
+			op := childNode.syncer.StartRead()
+			defer childNode.syncer.Complete(op)
+			if op.Async() {
+				return len(child.Children()) == 0
+			}
+			// TODO: ReadDir
+			return true
+		}()
+		if !checkChildEmpty {
+			return syscall.ENOTEMPTY
+		}
+
+		n.asyncOperation(ctx, &proto.OperationRequest{
+			Operation: &proto.OperationRequest_Rmdir{Rmdir: &proto.RmdirRequest{
+				Name: name,
+			}},
+		}, func(response *proto.OperationResponse, err error) {
+			defer n.syncer.CompleteAsync(op)
+			if err != nil {
+				log.Printf("Async Rmdir failed: %v", err)
+				return
+			}
+		})
+		n.RmChild(name)
+		return fs.OK
+	} else {
+		_, err := n.syncOperation(ctx, &proto.OperationRequest{
+			Operation: &proto.OperationRequest_Rmdir{Rmdir: &proto.RmdirRequest{
+				Name: name,
+			}},
+		})
+		if err != nil {
+			return fs.ToErrno(err)
+		}
+		n.RmChild(name)
+		return fs.OK
+	}
+}
+
+var _ = (fs.NodeUnlinker)((*Inode)(nil))
+
+func (n *Inode) Unlink(ctx context.Context, name string) syscall.Errno {
+	op := n.syncer.StartWrite()
+	defer n.syncer.Complete(op)
+	if op.Async() {
+		child := n.GetChild(name)
+		if child == nil {
+			return syscall.ENOENT
+		}
+		childNode, ok := child.Operations().(*Inode)
+		if !ok {
+			return syscall.EINVAL
+		}
+		if childNode.cachedStat.Mode&syscall.S_IFDIR != 0 {
+			return syscall.EISDIR // Cannot unlink a directory
+		}
+		n.asyncOperation(ctx, &proto.OperationRequest{
+			Operation: &proto.OperationRequest_Unlink{Unlink: &proto.UnlinkRequest{
+				Name: name,
+			}},
+		}, func(response *proto.OperationResponse, err error) {
+			defer n.syncer.CompleteAsync(op)
+			if err != nil {
+				log.Printf("Async Unlink failed: %v", err)
+				return
+			}
+		})
+		return fs.OK
+	} else {
+		_, err := n.syncOperation(ctx, &proto.OperationRequest{
+			Operation: &proto.OperationRequest_Unlink{Unlink: &proto.UnlinkRequest{
+				Name: name,
+			}},
+		})
+		if err != nil {
+			return fs.ToErrno(err)
+		}
+		return fs.OK
+	}
+}
+
+// TODO: Rename, Create, Open
+
+var _ = (fs.NodeOpener)((*Inode)(nil))
+
+func (n *Inode) Open(ctx context.Context, flags uint32) (fh fs.FileHandle, fuseFlags uint32, err syscall.Errno) {
+	op := n.syncer.StartWrite()
+	defer n.syncer.Complete(op)
+	if op.Async() {
+
+	}
+	return nil, 0, syscall.ENOSYS // Not implemented yet
+}
+
+var _ = (fs.NodeSymlinker)((*Inode)(nil))
+
+func (n *Inode) Symlink(ctx context.Context, pointedTo string, linkName string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
+	op := n.syncer.StartWrite()
+	defer n.syncer.Complete(op)
+	if op.Async() {
+		if n.GetChild(linkName) != nil {
+			return nil, syscall.EEXIST
+		}
+		newFileStat(ctx, &out.Attr, syscall.S_IFLNK|0777) // Symlink mode
+		stat := StatProtoFromAttr(&out.Attr)
+		node := NewLinkNode(n.serverProtocol, nil, SYNC_LOCK_READ, []byte(pointedTo), stat)
+		n.asyncOperation(ctx, &proto.OperationRequest{
+			Operation: &proto.OperationRequest_Symlink{Symlink: &proto.SymlinkRequest{
+				Name:   linkName,
+				Target: pointedTo,
+				Stat:   stat, // Include stat for symlink
+			}},
+		}, func(response *proto.OperationResponse, err error) {
+			defer n.syncer.CompleteAsync(op)
+			if err != nil {
+				log.Printf("Async Symlink failed: %v", err)
+				return
+			}
+			switch s := response.Response.(type) {
+			case *proto.OperationResponse_Symlink:
+				cookie := s.Symlink.Cookie
+				node.ResolveCookie(cookie)
+			default:
+				log.Printf("Received unexpected response type: %T", s)
+				return
+			}
+		})
+		return n.NewInode(ctx, node, fs.StableAttr{Mode: syscall.S_IFLNK | 0777}), 0
+	} else {
+		// TODO: Owner of symlink
+		response, err := n.syncOperation(ctx, &proto.OperationRequest{
+			Cookie: n.cookie,
+			Operation: &proto.OperationRequest_Symlink{Symlink: &proto.SymlinkRequest{
+				Name:   linkName,
+				Target: pointedTo,
+			}},
+		})
+		if err != nil {
+			return nil, fs.ToErrno(err)
+		}
+		switch s := response.Response.(type) {
+		case *proto.OperationResponse_Symlink:
+			out.Attr = *AttrFromStatProto(s.Symlink.Stat)
+			node := NewInode(n.serverProtocol, s.Symlink.Cookie, 0, s.Symlink.Stat)
+			return n.NewInode(ctx, node, fs.StableAttr{Mode: syscall.S_IFLNK | 0777}), 0
+		default:
+			log.Printf("Received response with SeqId %d but unknown type: %T, expecting SymlinkResponse", response.SeqId, s)
+			return nil, syscall.EIO
+		}
+	}
+}
+
+var _ = (fs.NodeSetattrer)((*Inode)(nil))
+
+func (n *Inode) Setattr(ctx context.Context, fh fs.FileHandle, attr *fuse.SetAttrIn, out *fuse.AttrOut) syscall.Errno {
+	op := n.syncer.StartWrite()
+	defer n.syncer.Complete(op)
+	if op.Async() {
+		if attr.Valid&fuse.FATTR_MODE != 0 {
+			n.cachedStat.Mode = attr.Mode
+		}
+		if attr.Valid&fuse.FATTR_UID != 0 {
+			n.cachedStat.Uid = attr.Uid
+		}
+		if attr.Valid&fuse.FATTR_GID != 0 {
+			n.cachedStat.Gid = attr.Gid
+		}
+		if attr.Valid&fuse.FATTR_SIZE != 0 {
+			n.cachedStat.Size = attr.Size
+		}
+		if atime, ok := attr.GetATime(); ok {
+			n.cachedStat.Atime = timestamppb.New(atime)
+		}
+		if mtime, ok := attr.GetMTime(); ok {
+			n.cachedStat.Mtime = timestamppb.New(mtime)
+		}
+		if ctime, ok := attr.GetCTime(); ok {
+			n.cachedStat.Ctime = timestamppb.New(ctime)
+		}
+		out.Attr = *AttrFromStatProto(n.cachedStat)
+		n.asyncOperation(ctx, &proto.OperationRequest{
+			Operation: &proto.OperationRequest_SetAttr{SetAttr: &proto.SetAttrRequest{
+				Stat: n.cachedStat,
+			}},
+		}, func(response *proto.OperationResponse, err error) {
+			defer n.syncer.CompleteAsync(op)
+			if err != nil {
+				log.Printf("Async Setattr failed: %v", err)
+				return
+			}
+			switch s := response.Response.(type) {
+			case *proto.OperationResponse_SetAttr:
+				out.Attr = *AttrFromStatProto(s.SetAttr.Stat)
+				n.cachedStat = s.SetAttr.Stat
+			}
+		})
+		return fs.OK
+	} else {
+		// TODO: Generate proto
+		response, err := n.syncOperation(ctx, &proto.OperationRequest{
+			Operation: &proto.OperationRequest_SetAttr{SetAttr: &proto.SetAttrRequest{
+				Stat: emptyFileStatProto(ctx, n.cachedStat.Mode),
+			}},
+		})
+		if err != nil {
+			return fs.ToErrno(err)
+		}
+		switch s := response.Response.(type) {
+		case *proto.OperationResponse_SetAttr:
+			out.Attr = *AttrFromStatProto(s.SetAttr.Stat)
+			n.cachedStat = s.SetAttr.Stat
+			return fs.OK
+		default:
+			log.Printf("Received response with SeqId %d but unknown type: %T, expecting SetAttrResponse", response.SeqId, s)
+			return syscall.EIO
+		}
+	}
+}
+
+// TODO: xattr
 
 func (n *Inode) asyncOperation(ctx context.Context, request *proto.OperationRequest, callback OpCallback) {
 	n.opMu.Lock()
---
+++ pkg/vfs/link_node.go
@@ -0,0 +1,52 @@
+package vfs
+
+import (
+	"context"
+	"log"
+	"syscall"
+
+	"github.com/hanwen/go-fuse/v2/fs"
+	"velda.io/mtfs/pkg/proto"
+)
+
+type LinkNode struct {
+	Inode
+	Target []byte // The target path of the symlink
+}
+
+func NewLinkNode(svc ServerProtocol, cookie []byte, flags int, target []byte, stat *proto.FileStat) *LinkNode {
+	node := &LinkNode{
+		Target: target,
+	}
+	node.init(svc, cookie, flags, stat)
+	return node
+}
+
+var _ = (fs.NodeReadlinker)((*LinkNode)(nil))
+
+func (n *LinkNode) Readlink(ctx context.Context) ([]byte, syscall.Errno) {
+	op := n.syncer.StartRead()
+	defer n.syncer.Complete(op)
+	if op.Async() {
+		return n.Target, fs.OK
+	}
+	response, err := n.syncOperation(ctx, &proto.OperationRequest{
+		Operation: &proto.OperationRequest_Readlink{
+			Readlink: &proto.ReadlinkRequest{},
+		},
+	})
+	if err != nil {
+		return nil, fs.ToErrno(err)
+	}
+	switch resp := response.Response.(type) {
+	case *proto.OperationResponse_Readlink:
+		if resp.Readlink == nil {
+			return nil, syscall.EIO
+		}
+		n.Target = resp.Readlink.Target
+		return resp.Readlink.Target, fs.OK
+	default:
+		log.Printf("ReadLink: Unexpected response type: %T", resp)
+		return nil, syscall.EIO
+	}
+}
