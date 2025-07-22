e5db74c4b12d773770c536cbd30e5ffdd88a9a1c
Chuan Qiu <qiuc12@gmail.com>
2025-07-22 10:35:01 -0700

    Add file stat in tests

--- pkg/test/concurrent_test.go
+++ pkg/test/concurrent_test.go
@@ -23,6 +23,9 @@ func TestConcurrentAccess(t *testing.T) {
 		_, err = file.Write([]byte("Hello, World!"))
 		assert.NoError(t, err, "Write to file should succeed")
 		assert.NoError(t, file.Close(), "Close file should succeed")
+		stat, err := os.Stat(dir1 + "/testdir/testfile.txt")
+		assert.NoError(t, err, "Stat file should succeed")
+		assert.Equal(t, stat.Size(), int64(13), "File size should match written data size")
 
 		// Rewrite on another client.
 		file2, err := os.Create(dir2 + "/testdir/testfile.txt")
@@ -30,10 +33,17 @@ func TestConcurrentAccess(t *testing.T) {
 		_, err = file2.Write([]byte("Another file content"))
 		assert.NoError(t, err, "Write to second file should succeed")
 		assert.NoError(t, file2.Close(), "Close file should succeed")
+		stat3, err := os.Stat(dir2 + "/testdir/testfile.txt")
+		assert.NoError(t, err, "Stat second file should succeed")
 
 		content, err := os.ReadFile(dir1 + "/testdir/testfile.txt")
 		assert.NoError(t, err, "Read second file should succeed")
 		assert.Equal(t, "Another file content", string(content), "Second file content should match written data")
+		stat2, err := os.Stat(dir2 + "/testdir/testfile.txt")
+		assert.NoError(t, err, "Stat second file should succeed")
+		assert.NotEqual(t, stat.ModTime(), stat2.ModTime(), "Modification times should differ after concurrent write")
+		assert.Equal(t, stat2.Size(), int64(20), "Second file size should match written data size")
+		assert.Equal(t, stat3.ModTime(), stat2.ModTime(), "Modification time of second file should match")
 
 		err = os.RemoveAll(dir1 + "/testdir")
 		assert.NoError(t, err, "Remove directory should succeed")
